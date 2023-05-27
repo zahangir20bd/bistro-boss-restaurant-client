8e1ad939d0aa9233422b1261dea0a8d5f56e6c2b
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-05-27 18:30:09 +0600

    Code modified

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import {
   loadCaptchaEnginge,
   LoadCanvasTemplate,
-  LoadCanvasTemplateNoReload,
   validateCaptcha,
 } from "react-simple-captcha";
 
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = useContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
